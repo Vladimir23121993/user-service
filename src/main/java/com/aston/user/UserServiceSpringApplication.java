ca36408b91968e7d70765fa590a1947a54b49255
Vladimir23121993 <e-ffra@mail.ru>
2026-07-21 01:38:33 +0300

    feat: добавлена интеграция с Eureka, Config Server, Gateway

--- src/main/java/com/aston/user/UserServiceSpringApplication.java
+++ src/main/java/com/aston/user/UserServiceSpringApplication.java
@@ -2,10 +2,11 @@ package com.aston.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceSpringApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserServiceSpringApplication.class, args);
     }
